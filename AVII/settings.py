ee46cfd4bfd4f20a4eff68b1df8b791438a262b5
KSHITIJ BARNWAL <73215352+kshibarn@users.noreply.github.com>
2023-02-23 22:38:47 +0530

    Alert messages settings

--- AVII/settings.py
+++ AVII/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -67,6 +68,14 @@ TEMPLATES = [
         },
     },
 ]
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 WSGI_APPLICATION = 'AVII.wsgi.application'
 
