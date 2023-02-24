dc75c6658ec113883c3754055f34d3385c749bf7
KSHITIJ BARNWAL <73215352+kshibarn@users.noreply.github.com>
2023-02-24 12:40:05 +0530

    Seperate login & signup views

--- accounts/signupviews.py
+++ accounts/signupviews.py
@@ -1,25 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.models import User, auth
+from django.contrib.auth.models import User
 
-# Create your views here.
-
-def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = auth.authenticate(username=username, password=password)
-
-        if user is not None:
-            auth.login(request, user)
-            return redirect("/")
-        else:
-            messages.info(request, 'Invalid Credentials')
-            return redirect('login')
-    else:
-        return render(request, "login.html")
-    
 def register(request):
     if request.method == 'POST':
         first_name = request.POST['first_name']
@@ -49,7 +31,3 @@ def register(request):
         return redirect('/')
     else:
         return render(request, "login.html")
-    
-def logout(request):
-    auth.logout(request)
-    return redirect('/')
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from . import views
+from AVII.accounts import signupviews
+
+from . import loginviews
 
 urlpatterns = [
-    path("register", views.register, name="register"),
-    path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout")
+    path("register", signupviews.register, name="register"),
+    path("login", loginviews.login, name="login"),
+    path("logout", loginviews.logout, name="logout")
 ]
