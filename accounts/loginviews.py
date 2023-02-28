dffd5ce970439824f891684a3c109c28c266d4c2
KSHITIJ BARNWAL <73215352+kshibarn@users.noreply.github.com>
2023-03-01 01:27:48 +0530

    Updated Comments

--- accounts/loginviews.py
+++ accounts/loginviews.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.models import auth
 
-# Create your views here.
-
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -11,15 +9,21 @@ def login(request):
 
         user = auth.authenticate(username=username, password=password)
 
+        # If the user exists, log the user in and redirect to home page
         if user is not None:
             auth.login(request, user)
             return redirect("/")
+        # If the user does not exist, display an error message and redirect to login page
         else:
             messages.info(request, 'Invalid Credentials')
             return redirect('login')
+    
+    # If request method is not POST, render the login page
     else:
         return render(request, "login.html")
     
 def logout(request):
+
+    # Log the user out and redirect to home page
     auth.logout(request)
     return redirect('/')
--- accounts/signupviews.py
+++ accounts/signupviews.py
@@ -3,7 +3,11 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 
 def register(request):
+
+    # If request method is POST, attempt to create a new user
     if request.method == 'POST':
+
+        # Extract relevant form data
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         username = request.POST['username']
@@ -11,23 +15,33 @@ def register(request):
         password2 = request.POST['confirmpass']
         email = request.POST['email']
 
+        # # If password1 and password2 match, attempt to create a new user
         if password1 == password2:
+
+            # If the username is already taken, redirect to register page with an error message
             if User.objects.filter(username=username).exists():
                 messages.info(request, 'Username Taken')
                 return redirect('register')
+            
+            # If the email is already taken, redirect to register page with an error message
             elif User.objects.filter(email=email).exists():
                 messages.info(request, 'Email Taken')
                 return redirect('register')
+            
+            # Create a new user with the provided data and redirect to login page
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('user created')
                 return redirect('login')
 
+        # If password1 and password2 do not match, redirect to register page
         else:
             messages.info(request, 'Password not matching..')
             return redirect('register')
 
         return redirect('/')
+    
+    # If request method is not POST, render the register page
     else:
         return render(request, "register.html")
