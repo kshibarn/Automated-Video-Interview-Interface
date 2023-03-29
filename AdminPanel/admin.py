5e7c07b2002a0a35e8f4a609a106226a8efdc39f
KSHITIJ BARNWAL <73215352+kshibarn@users.noreply.github.com>
2023-03-29 21:55:38 +0530

    Adding Models & admin site

--- AdminPanel/admin.py
+++ AdminPanel/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Set_Interview, Check_Interview
 
 # Register your models here.
+
+class SetInterviewAdmin(admin.ModelAdmin):
+    list_display = ('interview_name', 'company_name', 'last_date', 'time_limit')
+    search_fields = ('interview_name', 'company_name')
+    list_filter = ('last_date')
+
+class CheckInterviewAdmin(admin.ModelAdmin):
+    list_display = ('candidate_name', 'email_id', 'profile', 'gender', 'age', 'status_after_interview', 'applied_on', 'interview_details')
+    search_fields = ('candidate_name', 'email_id')
+    list_filter = ('status_after_interview', 'applied_on')
+
+admin.site.register(Set_Interview, SetInterviewAdmin)
+admin.site.register(Check_Interview, CheckInterviewAdmin)
--- AdminPanel/models.py
+++ AdminPanel/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.core.validators import EmailValidator
 
 # Create your models here.
+
+class Set_Interview(models.Model):
+    interview_name = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255)
+    last_date = models.DateField()
+    questions = models.TextField()
+    time_limit = models.IntegerField()
+
+    def __str__(self):
+        return self.interview_name
+    
+class Check_Interview(models.Model):
+    candidate_name = models.CharField(max_length=255)
+    email_id = models.CharField(max_length=255, validators=[EmailValidator()])
+    profile = models.CharField(max_length=255)
+    gender = models.CharField(max_length=10, choices=[('M', 'Male'), ('F', 'Female'), ('O', 'Other')])
+    age = models.PositiveIntegerField()
+    interview = models.ForeignKey(Set_Interview, on_delete=models.CASCADE)
+    status_after_interview = models.CharField(max_length=255, blank=True)
+    applied_on = models.DateTimeField(auto_now_add=True)
+    interview_details = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.candidate_name
